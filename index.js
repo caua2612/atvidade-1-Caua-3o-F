ae0c28bc63f8272c2c913db095a7bfb937f27040
caua2612 <167487865+caua2612@users.noreply.github.com>
2024-08-05 22:53:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const meuNome = "Cauã";
+const idade = "18";
+const altura = "1.75";
+const peso = "65";
+const sexo = "Masculino";
+const cidade = "São José";
+
+console.log(meuNome)
+console.log(idade)
+console.log(altura)
+console.log(peso)
+console.log(sexo)
+console.log(cidade)
